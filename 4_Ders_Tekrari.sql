6af7ef1c82d58deaf0ead4f4fbfa748adb8dfb72
tariksitki <tariksitki0119@gmail.com>
2021-12-31 16:37:38 +0100

    ...

--- 4_Ders_Tekrari.sql
+++ 4_Ders_Tekrari.sql
@@ -72,12 +72,72 @@ JOIN siparisler as s
 ON m.marka_id = s.marka_id;
 
 
+-- git branch -m yeni branchadi
+-- bu komut ile ana branch imizin adini degistiririz
+
+
+-- Normalde kullanim su sekildedir.
+-- markalar tablosu agirlikli:  left
+-- siparisler agirlikli :  right
+-- ortak noktalar:  inner
+-- hepsi   : full
+
+
+-- chinook veri tabaninda tracks tablosunda her bir sarkinin
+-- türü genre listeleyiniz
+-- yani genres tablosu ile tracks tablosu join
+
+SELECT *
+FROM genres;
+
+SELECT *
+FROM tracks;
+
+
+SELECT t.Name as song_name, g.Name as song_genre
+FROM tracks t 
+JOIN genres g 
+ON t.GenreId = g.GenreId;
+
+
+-- Burada left join de yapsak ayni sonucu
+-- alacagizdir. yani ilk tabloda olan hersey
+-- ikinci tabloda da bulunmaktadir
+-- yani null yok demektir
 
 
 
+-- Soru:  invoice tablosunda faturalarin her birinin
+-- first name lastname fatura tarihi
+ -- ve fatura meblagini total
+-- listeleyen sorgu yaziniz
+-- diger tablo customers olmali
+
+SELECT *
+from invoices;
+
+SELECT *
+FROM customers;
 
 
 
+SELECT c.FirstName, c.LastName, i.InvoiceDate, i.Total
+FROM customers c 
+JOIN invoices i 
+on c.CustomerId = i.CustomerId;
+
+
+-- Burada da left join yaparsak birsey degismez
+
+-- simdi de ayni soruyu, isim ve soyisimleri gruplandirip
+-- yapalim.. yani her bir isim ve soyismin toplami ne kadar
+-- onu bulalim
+
+SELECT c.FirstName, c.LastName, sum(i.Total)
+FROM customers c 
+JOIN invoices i 
+on c.CustomerId = i.CustomerId
+GROUP BY c.FirstName, c.LastName;
 
 
 
