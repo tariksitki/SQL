9acbfcaa9a539051ec00e9eb2ffa4998edc722a7
tariksitki <tariksitki0119@gmail.com>
2022-01-01 22:45:44 +0100

    ...

--- 4_Ders_Tekrari.sql
+++ 4_Ders_Tekrari.sql
@@ -683,9 +683,522 @@ CREATE TABLE calisanlar
 
 -- simdi sqlite da database structure diyerek tabloyu inceleyelim
 
--- 
+-- Not: Tablo olusturmak icin kod yazmak zorunda degiliz
+-- database structure da modify diyerek orada tabloyu 
+-- sekillendirebilir yeni sütunlar satirlar ekleyebiliriz
 
 
+
+-- sqlite da modify dedigimizde, burada tablonun sütunlari
+-- üzerinde ayarlama yapabiliriz
+-- Burada bulunan ifadelerin anlamlari
+
+-- NN:  Not null
+-- PK:  Primary key
+-- AI:  Autoincrement
+-- U :  Unique
+
+-- Add tusu ile yeni sütun eklenir
+
+-- Burada bir sütunu foreign key yapma sansimiz da vardir
+
+-- sari renk: primary key
+-- gri renk: foreign key
+
+
+-- Database structure a tikladigimizda, ekranin sol üstünde
+-- create table tusu bulunur. Bu tus ile yeni bir tablo
+-- sifirdan olusturulabilir (kod yazmadan)
+
+-- Burada tablo olusturdugumuzda, yine buradaki arayüz de 
+-- otomatik olarak bizim olusturdugumuz tablonun kodlari da
+-- yazilacaktir. eger kod yazmayi unutursak, arayüz vasitasi ile
+-- bir tablo olustururuz ve burada cikan kodlara bakarak
+-- baska bir tablo olusturabiliriz
+
+-- vs code da ise sadece kod ile yapma secenegimiz vardir
+
+
+-- tablo olusturduktna sonra ayni kodu bir daha calistirirsak
+-- bu durumda hata aliriz. ayni tabloyu birdaha olusturmamasi
+-- aslinda güzel bir seydir
+
+
+-- var olan bir tabloya satir yani row ekleme:
+
+-- browse data secenegine tiklariz
+-- Burada tablomuzu seceriz
+-- printer isaretinin sagindaki "insert a new row"
+--secenegine tiklayarak yeni bir row ekleriz
+
+-- eklenen bu row a ister iki kez tiklayarak sagda acilan
+-- pencerede degerleri gireriz. yada tek tiklayarak 
+-- direkt yazabiliriz
+
+
+-- eger tablo olusturdugumuz kodu tekrar calistirdigimizda hata
+-- almak istemiyorsak bu durumda su kod kullanilabilir:
+
+CREATE TABLE IF NOT EXISTS calisanlar
+    (first_name text,
+    last_name text,
+    age INT,
+    hire_date Date);
+
+
+
+
+
+--- Soru: vacation_plan adinda bir tablo olusturalim
+-- sütun isimleri su sekilde olsun:
+
+-- place_id, country,  hotel_name, employee_id, 
+-- vacation_length, budget
+
+
+
+CREATE TABLE IF NOT EXISTS vacation_plan
+    (place_id integer,
+    country varchar(20),
+    hotel_name text,
+    employee_id integer,
+    vacation_length integer,
+    budget integer);
+
+
+
+
+
+
+
+
+----------------  Tablo silme:
+
+
+DROP TABLE personel;
+
+-- simdi alttaki komut hata verir
+
+SELECT *
+FROM personel ;
+
+
+-- eger silme komutu tekrar calistiginda hata almak istemezsek
+-- If Exists komutu kullanilabilir
+
+
+DROP TABLE IF EXISTS personel;
+
+-- dikkat: if existy hemen table dan sonra kullanilir
+
+
+
+
+
+
+-- Tabloya kod ile veri girme:
+
+
+
+--- Truncate komutu SQLite da bulunmasina ragmen 
+-- bu komutu desteklemez. Peki bu komut ne ise yarar
+-- TRUNCATE TABLE tablename komutu ile bir tablo icerisindeki
+-- tüm verileri sileriz ancak tablo kalir
+
+
+-- Biz bu komut yerine delete kullaniyoruz
+
+
+
+
+
+
+----------------------  Insert into
+
+--- Bir tabloya komut ile veri girme
+-- syntax su sekildedir
+
+INSERT INTO table_name
+VALUES();
+
+-- bu kodu kullanirken dikkatli olmamiz gerekir:
+
+-- tablo olustururken hangi siralama ile verileri yazdiysak
+-- o siralama ile verileri girmemiz gerekir. eger yanlis siralama
+-- ile girersek veri tipi uyusmayabilir
+
+-- veri sayisi da tablodaki ile ayni olmalidir
+-- yani sütun sayisi kadar veri  girilir
+
+-- ve her bir veri virgül ile ayrilir
+
+
+-- simdi vacation tablosuna veri girelim:
+-- tablo su sekilde idi:
+CREATE TABLE IF NOT EXISTS vacation_plan
+    (place_id integer,
+    country varchar(20),
+    hotel_name text,
+    employee_id integer,
+    vacation_length integer,
+    budget integer);
+
+
+-- Not: degerler girilirken degerlerin ait oldugu sütun isimleri
+-- yazilmaz.... sadece girmek istedigimiz degerler sira ile yazilir
+
+INSERT INTO vacation_plan
+VALUES ("1234",
+        "Holland",
+        "marriott",
+        "1234",
+        "10",
+        12345);
+
+
+SELECT *
+FROM vacation_plan;
+
+
+-- Not: Biz tablomuza henüz bir primary key veya foreign key koymadik.
+-- dolayisi ile herhangi bir kisitlamamiz yok. Bu nedenle yukaridaki 
+-- veri girme kodunu kac kez yazarsak o kadar row yerlestirmis oluruz. 
+
+
+
+
+-- veri girerken herhangi bir row icin null girebiliriz ancak
+-- hicbir degeri bos birakamyiz hata verir
+
+
+-- kisitlama koymadigimiz sürece null girilebilir
+
+-- not null olsaydi giremezdik
+
+
+
+-- Bir tabloda sadece istedigimiz satirlara veri girme sansimiz da vardir
+-- mesela ayni tabloda son iki satir haric digerlerine veri girelim:
+-- tablo su sekilde idi:
+
+CREATE TABLE IF NOT EXISTS vacation_plan
+    (place_id integer,
+    country varchar(20),
+    hotel_name text,
+    employee_id integer,
+    vacation_length integer,
+    budget integer);
+
+
+INSERT INTO vacation_plan(place_id,country,hotel_name,employee_id)
+VALUES ("abcde", "USA", "hilton", "123");
+
+
+SELECT *
+FROM vacation_plan;
+
+
+-- Bu kodu calistirdigimizda, veri girmedigimiz satirlar yerine
+-- Null girilecektir
+
+-- Bu yöntemin güzelligi, ilk parantez icerisinde sütun isimlerini
+-- yazarken siralama ile yazmak zorunda degiliz
+
+
+-- isimlerin dogru olmasina ve ikinci parantezde siralamanin
+-- dogru olmasina dikkat etmeliyiz
+
+
+
+------------- Primary key
+
+CREATE TABLE workers (
+    id int PRIMARY KEY,
+    id_nummer varchar(11) UNIQUE NOT NULL);
+
+-- bu kod da 3 farkli keyword gördük.
+-- primary key
+-- unique: tekrari olmasin istiyoruz
+-- not null: tc kimlik no gibi
+
+-- primary key hem unique hem de not null'i barindirir
+-- bu nedenle ayri olarak yazmiyoruz
+
+
+
+CREATE TABLE workers (
+    id int PRIMARY KEY,
+    id_nummer varchar(11) UNIQUE NOT NULL,
+    name text DEFAULT "isimsiz");
+
+
+-- burada ise default'u kullandik. eger bos birakilirsa
+-- o zaman default atansin deriz
+
+
+
+------------ Primary key (farkli kullanim)
+
+CREATE TABLE workers (
+    id int ,
+    id_nummer varchar(11) UNIQUE NOT NULL,
+    PRIMARY KEY("id"));
+
+-- Burada id'i yukarida tanimladik ancak PK oldugunu
+-- en altta yazdik. 
+
+
+
+---------- simdi bu tabloya veri girelim:
+
+
+INSERT INTO workers(1, "12345", "Ahmet", 7000);
+
+
+INSERT INTO workers(2, "12345", "Mehmet", 8000); -- 2. deger unique olmali Hata
+
+
+INSERT INTO workers(id, id_nummer, salary)   -- ismi default olarak atar
+VALUES (3, "1234567", "Ali", 5000);
+
+
+
+
+
+---- Soru: vacation_plan tablosunu place_id sütunu
+-- PK ve employee_id sütununu ise FK olarak degistirerek
+-- vacation_plan2 adinda yeni bir tablo olusturalim. 
+-- Bu tablo, employees tablosu ile iliskili olmalidir
+
+
+CREATE TABLE IF NOT EXISTS vacation_plan2
+    (place_id integer,
+    country varchar(20),
+    hotel_name text,
+    employee_id integer,    
+    vacation_length integer,
+    budget integer,
+    PRIMARY KEY("place_id"),
+    FOREIGN KEY("employee_id") REFERENCES employees(EmployeeId));  -- dikkat: burada sadece 
+                                                                    -- iliskilendirecegimiz tablonun
+                                                                    -- ismi degil. sütun adi da yazilir
+
+
+-- employees tablosunda, employee_id integer oldugu icin bizde integer yapiyoruz
+-- yani refere ettigimiz tablo ile bizim verimiz ayni tip de olmak zorundadir
+
+
+-- asagidaki kod da, ekstradan id adinda bir sütun belirledik ve bunu primary key yaptik. 
+-- ve bu otomatik olarak kendisi artan bir deger olacaktir. Bu durumda biz tabloya her veri
+-- girdigimizde, bu sütun kismini bos birakacagiz, o kendisi girecektir. Bu demek oluyor ki
+-- Biz her seferinde parcali veri girisi yapacagizdir
+
+
+-- Not: FK ve PK girerken bir tabloda employee_id
+-- diger tabloda ise EmployeeId olarak girdik.
+-- Ancak bu veriler FK ve PK olarak birbiri ile iliskili
+-- Bu nedenle sikinti olmaz. 
+
+CREATE TABLE IF NOT EXISTS vacation_plan2
+    (id integer
+    place_id integer,
+    country varchar(20),
+    hotel_name text,
+    employee_id integer,    
+    vacation_length integer,
+    budget integer,
+    PRIMARY KEY("id" AUTOINCREMENT),
+    FOREIGN KEY("employee_id") REFERENCES employees);
+
+
+--- foreign key ile baglanan tablolarda, veri girisi veya degisikliklerde
+-- dikkatli olmamiz gerekir. Bu tablolar arasinda parent child iliskisi 
+-- vardir. eger biz parent'i silmek istersek izin vermez. önce parent'i
+-- sil daha sonra child'i sil der
+
+
+-- Soru: employees tablosundaki EmployeeId'si
+-- 1 olan kisi icin bir tatil plani giriniz:
+
+
+--- tablomuz su sekilde idi:
+
+CREATE TABLE IF NOT EXISTS vacation_plan2
+    (place_id integer,
+    country varchar(20),
+    hotel_name text,
+    employee_id integer,    
+    vacation_length integer,
+    budget integer,
+    PRIMARY KEY("place_id"),
+    FOREIGN KEY("employee_id") REFERENCES employees(EmployeeId));
+
+
+INSERT INTO vacation_plan2
+VALUES(3, "TR", "Happy Hotel",1, 5, 5660);
+
+
+
+-- Employees tablosunda toplam da 8 tane employeeId vardir.
+-- ve bu sütun bizim PK mizdir. Eger biz kendi tablomuzdaki FK
+-- yani employee_id sütununa 9 kisiyi girmek istersek hata aliriz. 
+-- fk ve pk sütunundaki sayi adedi farkli olamaz
+-- yani asagidaki kod hata verir
+
+INSERT INTO vacation_plan2
+VALUES(4, "TR", "Happy Hotel",9, 5, 5660);
+
+
+---- önemli: Bizim buradaki place_id miz sadece
+-- bizim tablomudaki verinin siralamasini gösteren
+-- bir sayidir. yani diger tablolar ile bir iliskisi yoktur. 
+-- ancak unique olmak zorundadir. 
+
+
+-- employee_id ise bizim tablomuzun diger tablolar ile 
+-- iliskisini saglayan veridir. Bu nedenle veri girerken
+-- eger place_id yi unique olarak giriyorsak, employee_id yi
+-- birden cok kez ayni rakam girebiliriz. cünkü bir kisi 
+--- birden cok kez farkli farkli yerlere tatile gitmis olabilir. 
+
+
+--- yani su kod calisir
+
+INSERT INTO vacation_plan2
+VALUES(5, "TR", "Happy Hotel",1, 5, 5660);
+
+
+
+--- sql genel ekraninin sol üstünde "new database" butonu bulunur. 
+-- Bu butona tiklayarak yeni bir database olsuturup burada yeni
+-- tablolar olusturabiliirz
+
+
+
+-- simdi join ile iki tablodan veri cekelim
+
+SELECT vacation_plan2.employee_id, vacation_plan2.place_id, employees.FirstName, employees.LastName
+FROM vacation_plan2
+JOIN employees
+on vacation_plan2.employee_id = employees.EmployeeId;
+
+
+--- Burada sart da kullanabilirdik:
+
+SELECT vacation_plan2.employee_id, vacation_plan2.place_id, employees.FirstName, employees.LastName
+FROM vacation_plan2
+JOIN employees
+on vacation_plan2.employee_id = employees.EmployeeId and vacation_plan2.employee_id = 1;
+
+
+--- create islemi, CRUD operasyonlarindaki create islemidir
+
+
+
+
+
+------------------ Alter Table: (ADD; RENAME TO; DROP)
+
+-- ADD komutu ile tablomuza yeni bir sütun ekleriz
+
+ALTER TABLE vacation_plan2
+ADD Name DEFAULT "isimsiz";
+
+
+
+
+-----------  eklenmis bir sütunu silme
+--- DROP COLUMN 
+
+-- Bu komut bazi pc lerde calismiyor
+
+ALTER TABLE vacation_plan2 -- tablo adi
+DROP COLUMN name;          -- column name
+
+
+--- Not: diger sql platformlarinda alter islemleri cok
+-- genistir. mesela foreign key ler bile degistirilebilir
+
+-- ancak sqlite da bu islemler cok kisitlidir
+
+
+--- workers tablosunun adini people olarak degistirelim
+
+
+
+------------- tablo ismi degistirme
+
+
+ALTER TABLE workers
+RENAME to "people";   -- burada parantezli de olur parantezsiz de
+
+-- burada to önemli
+-- dikkat rename ile tablonun ismini degistiririz
+
+
+
+
+---------------  Update, Delete (Bu komutlar, tablo icerisindeki bir veya birkac veri ile ilgilidir
+                                -- Komple tabloyu silme islemi degildir)
+
+
+UPDATE tablo_adi
+SET sütun1 = yeni_deger1, sütun2 = yeni_deger2, ..... 
+WHERE kosul 
+
+
+-- Eger burada where ile kosul girmezsek, bu durumda
+-- sütundaki tüm verileri degistirecektir
+
+
+
+
+-------------------------  Delete:
+
+DELETE tablo_name
+WHERE Kosul;                --- ayni sekilde burada da kosul kullanmazsak tüm veriler silinir
+
+
+
+-- Not: mesela önceden tablomuda bir Foreign key vardi ve biz
+-- su an bunu degistirmek yada tamamen kaldirmak istiyoruz
+-- bu durum alter'in kapsamina girer. 
+-- Cünkü burada tablonun ana yapisini degistiriyoruz ana kaporta degisir
+-- bu islem update degildir
+-- update de ise sadece birkac veri degisir
+
+
+-- mesela foreign key sütununda 1 olan bir degeri 2 yapmak istiyorsak
+-- bu durumda update kullanilabilir
+-- ancak foreign key sütununu komple degistirmek istiyorsak,
+-- bu durumda alter kullanilir
+
+
+
+UPDATE vacation_plan2
+SET hotel_name = "komagene hotel"
+WHERE employee_id = 1;
+
+
+
+-- people tablosunda salary sütunu 5000 den fazla olanlarin
+-- maasina 10% zam yapacak sorguyu yazalim
+
+
+UPDATE people
+SET salary = salary * 1.1
+WHERE salary > 5000;
+
+
+--------------------------  delete
+
+--- asagidaki kodu yazdigimizda, sartin saglandigi
+-- row u komple siler
+
+
+
+DELETE FROM vacation_plan2
+WHERE employee_id = 1;
 
 
 
